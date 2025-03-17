d5cfe101c11fbb69a6782985c0bd0920ea976a04
Nanda Vardhan <nandavardhan09@gmail.com>
2025-03-17 16:05:38 +0530

    Remove all files from mserver repository

--- routes/results.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const resultsController = require('../controllers/resultsController');
-
-router.get('/:studentId', resultsController.getResultByStudentId);
-
-module.exports = router;
